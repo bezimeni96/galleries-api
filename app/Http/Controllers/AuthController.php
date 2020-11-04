e7cf6fe67a8042ade4fc604ecd05aa95fd524ca3
Dusan Vostic <vosticdusan@gmail.com>
2020-11-04 19:13:42 +0100

    U AuthController dodata metoda register sa validacijom RegisterRequest.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Support\Str;
+
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -27,5 +32,19 @@ class AuthController extends Controller
 
     public function logout() {
         return auth('api')->logout(true);
+    }
+
+    public function register(RegisterRequest $request)
+    {
+        $data = $request->validated();
+        $user = User::create([
+            "first_name" => $data['first_name'],
+            "last_name" => $data['last_name'],
+            "email" => $data['email'],
+            "email_verified_at" => now(),
+            "password" => Hash::make($data['password']),
+            "remember_token" => Str::random(10),
+        ]);
+        return $user;
     }
 }
